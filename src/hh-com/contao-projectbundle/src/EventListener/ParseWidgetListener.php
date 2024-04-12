971f2a15df08714836abe2d88340bdbceeab13e1
hh-com <info@harald-huber.com>
2024-04-12 14:49:26 +0200

    be form grid update

--- src/hh-com/contao-projectbundle/src/EventListener/ParseWidgetListener.php
+++ src/hh-com/contao-projectbundle/src/EventListener/ParseWidgetListener.php
@@ -18,8 +18,9 @@ class ParseWidgetListener
         // Add the row and col class to the form_fields in Backend
         if (Input::get('table') == "tl_form_field" && Input::get('act') != "edit") {
 
-            $replaceWith = 'data-gridstyle="'.trim($widget->grid_css).'" class=" begridhelper ';
-            $buffer = preg_replace('/class="/', $replaceWith , $buffer, 1);
+            #$replaceWith = 'data-gridstyle="'.trim($widget->grid_css).'" class=" begridhelper ';
+            #$buffer = preg_replace('/class="/', $replaceWith , $buffer, 1);
+            $buffer = $buffer . '<span class="begridhelper" data-gridstyle="'.trim($widget->grid_css).'">'.$widget->grid_css.'</span>';
         }
 
         return $buffer;
